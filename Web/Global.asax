3bee898b00dfd9fc26c6dbc143cfa2d807a502d2
Mike Chung <mikechung@live.com>
2017-09-11 12:06:37 -0700

    create blank web project.

---
+++ Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwitterReport.Web.MvcApplication" Language="C#" %>
---
+++ Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwitterReport.Web.Startup))]
+namespace TwitterReport.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
